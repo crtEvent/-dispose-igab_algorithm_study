dd8fd0f5824ed20123db62610789a6cd35db3ed4
sejeong00 <buttermelt@daum.net>
2023-03-01 11:40:27 +0900

    [BOJ] sejeong00 / 카드2 / 실버4 related to #14

---
+++ sejeong00/doit/ch03_스택_큐/boj2164/Main.java
@@ -0,0 +1,26 @@
+package doit.ch03_스택_큐.boj2164;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer;
+
+        Queue<Integer> queue= new LinkedList<>();
+
+        for(int i = 1; i <= n; i++){
+            queue.add(i);
+        }
+
+        while(queue.size() > 1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
